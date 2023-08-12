29b63cfdf42f236873596a09c13705b256839a52
amspsingh04 <sp.singh.150604@gmail.com>
2023-08-13 00:55:17 +0530

    modified:   dataset/scrape0.py 	new file:   dataset/scrape1_3.py 	new file:   geckodriver.log 	new file:   leetcode_questions.csv 	new file:   leetcode_questions_bfs.csv 	new file:   leetcode_questions_binary_search.csv 	new file:   leetcode_questions_database.csv 	new file:   leetcode_questions_dynamic_programming.csv 	new file:   leetcode_questions_hash_table.csv 	new file:   leetcode_questions_matrix.csv 	new file:   leetcode_questions_sorting.csv 	new file:   leetcode_questions_string.csv 	new file:   leetcode_questions_tree.csv

--- dataset/scrape0.py
+++ dataset/scrape0.py
@@ -4,31 +4,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 import time
+import csv
 
 
 # URL of the page with questions
-url = 'https://leetcode.com/problemset/all/?page=1&topicSlugs=array'
 gecko= 'C:/Users\spsin\Desktop\geckodriver-v0.33.0-win64\geckodriver'
 
 # XPath expression for the elements containing the text
-xpath_expression = f'/html/body/div[1]/div/div[2]/div[1]/div[1]/div[5]/div[2]/div/div/div[2]/div[{i}]/div[2]/div/div/div/div/a'
 options = Options()
+arr_tags=['Matrix', 'Binary-Tree', 'Two-Pointers', 'Bit-Manipulation', 'Heap', 'Stack','Prefix-Sum', 'Graph', 'Simulation', 'Design', 'Backtracking', 'Counting', 'Sliding-Window', 'Union-Find', 'Linked-List', 'Ordered-Set', 'Enumeration']
+for name in arr_tags:
+    csv_filename = f'leetcode_questions_{name}.csv'
+    csv_file = open(csv_filename, 'w', newline='', encoding='utf-8')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Question'])
+    nm=name
+    # Initialize the Firefox WebDriver
+    service = Service(executable_path=gecko, log_path='geckodriver.log')
+    driver = webdriver.Chrome()
+    for j in range(8):
+        url = f'https://leetcode.com/problemset/all/?page={j}&topicSlugs={nm}'
+            # Navigate to the URL
+        driver.get(url)
+        time.sleep(6)
+                # Find elements using XPath
+        for i in range(1,51):
+            xpath_expression = f'/html/body/div[1]/div/div[2]/div[1]/div[1]/div[5]/div[2]/div/div/div[2]/div[{i}]/div[2]/div/div/div/div/a'
+            element = driver.find_element(By.XPATH, xpath_expression).text
+            csv_writer.writerow([element])
+    # Extract and print the text from each element, stripped of whitespace
 
-# Initialize the Firefox WebDriver
-service = Service(executable_path=gecko, log_path='geckodriver.log')
-driver = webdriver.Chrome()
 
-
-# Navigate to the URL
-driver.get(url)
-time.sleep(5)
-
-# Find elements using XPath
-element = driver.find_element(By.XPATH, xpath_expression)
-print(element.text)
-
-# Extract and print the text from each element, stripped of whitespace
-
-
-# Close the browser
-driver.quit()
+    # Close the browser
+    driver.quit()
