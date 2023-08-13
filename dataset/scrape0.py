39e1b2c4f42480c18220b3c225b4b906d2121c35
amspsingh04 <sp.singh.150604@gmail.com>
2023-08-13 14:42:23 +0530

    Changes to be committed:

--- dataset/scrape0.py
+++ dataset/scrape0.py
@@ -12,9 +12,9 @@ gecko= 'C:/Users\spsin\Desktop\geckodriver-v0.33.0-win64\geckodriver'
 
 # XPath expression for the elements containing the text
 options = Options()
-arr_tags=['Depth-First-Search']
+arr_tags=['biconnected-component']
 for name in arr_tags:
-    csv_filename = f'leetcode_questions_{name}.csv'
+    csv_filename = f'source_dataset/leetcode_questions_{name}.csv'
     csv_file = open(csv_filename, 'w', newline='', encoding='utf-8')
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(['Question','Tag'])
@@ -22,16 +22,17 @@ for name in arr_tags:
     # Initialize the Firefox WebDriver
     service = Service(executable_path=gecko, log_path='geckodriver.log')
     driver = webdriver.Chrome()
-    for j in range(7):
-        url = f'https://leetcode.com/problemset/all/?page={j}&topicSlugs=depth-first-search'
+    for j in range(1,2):
+        url = f'https://leetcode.com/problemset/all/?page=1&topicSlugs={name}'
             # Navigate to the URL
         driver.get(url)
         time.sleep(6)
                 # Find elements using XPath
-        for i in range(1,51):
+        for i in range(1,5):
             xpath_expression = f'/html/body/div[1]/div/div[2]/div[1]/div[1]/div[5]/div[2]/div/div/div[2]/div[{i}]/div[2]/div/div/div/div/a'
             element = driver.find_element(By.XPATH, xpath_expression).text
-            csv_writer.writerow([element,'Array'])
+            question_title = ' '.join(element.split('.')[1:]).strip()  # Stripping the numbering
+            csv_writer.writerow([question_title, f'{name}'])
     # Extract and print the text from each element, stripped of whitespace
 
 
--- dataset/set_cat.py
+++ dataset/set_cat.py
@@ -1,7 +1,7 @@
 import csv
 
 # List of source CSV files
-source_files = ['leetcode_questions_Array.csv', 'leetcode_questions_string.csv', 'leetcode_questions_Hash-Table.csv', 'leetcode_questions_dynamic_programming.csv', 'leetcode_questions_Math.csv', 'leetcode_questions_sorting.csv', 'leetcode_questions_Greedy.csv', 'leetcode_questions_Depth-First-Search.csv', 'leetcode_questions_database.csv', 'leetcode_questions_binary_search.csv', 'leetcode_questions_bfs.csv', 'leetcode_questions_tree.csv']  # Replace with your file names
+source_files = ['array', 'string', 'hash-table', 'dynamic-programming','math','sorting','greedy','depth-first-search','database','binary-search','breadth-first-search','tree', 'matrix', 'binary-tree', 'bit-manipulation', 'heap', 'stack' ,'prefix-sum', 'graph', 'simulation', 'design', 'backtracking', 'counting', 'sliding-window', 'union-find', 'linked-list', 'ordered-set','enumeration','monotonic-stack','trie','recursion','divide-and-conquer','bitmask','number-theory','queue','binary-search-tree','segment-tree','memoization','geometry','topological-sort','binary-indexed-tree','hash-function','game-theory','shortest-path','interactive','combinatorics','data-stream','string-matching','rolling-hash','brainteaser','randomized','monotonic-queue','merge-sort','iterator','concurrency','doubly-linked-list','probability-and-statistics','quickselect','bucket-sort','suffix-array','minimum-spanning-tree','counting-sort','shell','line-sweep','reservoir-sampling','eulerian-circuit','radix-sort','strongly-connected-component','rejection-sampling','biconnected-component']
 
 concatenated_file = 'concatenated_data.csv'
 
@@ -11,7 +11,7 @@ with open(concatenated_file, 'w', newline='', encoding='utf-8') as new_csvfile:
 
     # Loop through each source CSV file
     for source_file in source_files:
-        with open(source_file, 'r', newline='', encoding='utf-8') as source_csv:
+        with open(f"leetcode_questions_{source_file}.csv", 'r', newline='', encoding='utf-8') as source_csv:
             csv_reader = csv.reader(source_csv)
             
             # Skip the header if it exists in source files
